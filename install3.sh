26d01f16e6138858f0fa1129e4b0dc5a9f9f4cc8
dasssssssss <91534432+dasssssssss@users.noreply.github.com>
2021-09-28 15:49:31 +0200

    Update install3.sh

--- install3.sh
+++ install3.sh
@@ -1,2 +1,2 @@
 dhclient
-sudo pacman -S nmap openvpn i3 xorg-server xorg-apps compton thunar alsa-utils pavucontrol vlc firefox rxvt-unicode
+sudo pacman -S nmap openvpn i3 xorg-server xorg-apps xorg-xinit compton thunar alsa-utils pavucontrol vlc firefox rxvt-unicode
