a7acc13b4315fb296267b1e65309434000338e70
dasssssssss <91534432+dasssssssss@users.noreply.github.com>
2021-09-29 11:43:49 +0200

    Create copy_files.sh

---
+++ copy_files.sh
@@ -0,0 +1,9 @@
+cp .fehbg ~/
+cp .xinitrc ~/
+cp .Xresources ~/
+cp display.sh ~/
+cp vpn.sh ~/
+cp i3config ~/.config/i3/
+mv ~/.config/i3/i3config ~/.config/i3/config
+mkdir ~/protonvpn
+cp nl.protonvpn.com.udp.ovpn ~/protonvpn/
